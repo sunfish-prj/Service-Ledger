d456bb53065728c5110674482ed7cdc516734de2
Andrea Margheri <margheri.andrea@gmail.com>
2017-08-02 16:00:29 +0200

    fix config

--- server/service/PutService.js
+++ server/service/PutService.js
@@ -11,7 +11,7 @@ exports.putPOST = function(putSpec) {
   return new Promise(function(resolve, reject) {
     var examples = {};
     examples['application/json'] = {
-  "message" : "aeiou"
+  "message" : "aeiou-PUT"
 };
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
