a36d1963374204e85a3663688bd2a05de890a95d
Andrea Margheri <margheri.andrea@gmail.com>
2017-08-02 15:25:34 +0200

    removed docs from this repo

--- generatecode.sh
+++ generatecode.sh
@@ -3,15 +3,15 @@
 #Creation of Server-stub
 #swagger-codegen generate -i ./server/api/registry.yaml -l  -o ./server/
 java -jar ~/workspace/swagger/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-   -i ./swagger/registry.yaml \
+   -i ./API/registry.yaml \
    -l nodejs-server \
    -o ./server/
 
 #Documentation
 #swagger-codegen generate -i ./server/api/registry.yaml -l dynamic-html -o .
 
-java -jar ~/workspace/swagger/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-   -i ./swagger/registry.yaml \
-   -l dynamic-html \
-   -o .
+#java -jar ~/workspace/swagger/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+#   -i ./API/registry.yaml \
+#   -l dynamic-html \
+#   -o .
 
