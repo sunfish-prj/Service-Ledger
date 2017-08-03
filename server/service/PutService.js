a298f568c03f7a727d4de5f07d409ed75ee5313b
fede <lombardi@dis.uniroma1.it>
2017-08-03 13:56:49 +0100

    draft hyperledger setting

--- server/service/PutService.js
+++ server/service/PutService.js
@@ -1,6 +1,12 @@
 'use strict';
 
+// output service configuration
+var config = require('config');
+var out_service_name = config.get('out-service.name');
+
 var db_utils = require('../utils/dbUtils.js');
+
+
 
 /**
  * Storing a key-value pair 
@@ -11,14 +17,27 @@ var db_utils = require('../utils/dbUtils.js');
 exports.putPOST = function(putSpec) {
     var message = {};	
     return new Promise(function(resolve, reject) {
- 	  db_utils.db_put(putSpec, function(res){	
-		  if (Object.keys(res).length > 0) {	
-	  		message = JSON.stringify({"message" : res});
-	  		console.log(message);
-			resolve(message);
-		  }else{
-			reject(message);
-		  }
-	  });	  
+ 	  if (out_service_name == 'mongo') {
+		  db_utils.db_put(putSpec, function(res){	
+			  if (Object.keys(res).length > 0) {	
+		  		message = JSON.stringify({"message" : res});
+		  		console.log(message);
+				resolve(message);
+			  }else{
+				reject(message);
+			  }
+	  	});
+	  }
+	  if (out_service_name == 'fabric') {
+          	hl_utils.db_put(putSpec, function(res){	
+			  if (Object.keys(res).length > 0) {	
+		  		message = JSON.stringify({"message" : res});
+		  		console.log(message);
+				resolve(message);
+			  }else{
+				reject(message);
+			  }
+		  });
+	  }  
     });
 }
---
+++ server/utils/hlUtils.js
@@ -0,0 +1,93 @@
+
+// output service configuration
+var config = require('config');
+var assert = require('assert');
+var out_service_ip = config.get('out-service.ip');
+var out_service_port = config.get('out-service.port');
+var db_name = config.get('out-service.dbname');
+var db_collection = config.get('out-service.dbcollection');
+
+var MongoClient = require( 'mongodb' ).MongoClient;
+var url = 'mongodb://' + out_service_ip + ':' + out_service_port + '/' + db_name;
+
+
+/* Hyperledger Fabric - PUT */
+var hl_put = exports.hl_put =  function(myobj, callback) {	
+    MongoClient.connect(url, function(err, db) {
+	  assert.equal(null, err);
+      console.log("Connected successfully to mongodb");
+	  _put(myobj, db, function(res){
+	      db.close();
+ 	  	  return callback(res);
+	  });
+    });
+}
+
+function _put (myobj, db, callback){
+	console.log('Executing put');
+	var collection = db.collection(db_collection);
+	// Insert a document
+	collection.update({"key": myobj.key},
+		 	myobj, {upsert: true}, function(err, res){
+    	if (err) {
+			console.log(err);
+			return new Error();
+		}
+		console.log('Put succeeded');
+		return callback("ok");
+   })
+}
+
+
+/* Hyperledger Fabric - GET */
+var hl_get = exports.hl_get =  function(_id, callback) {	
+    MongoClient.connect(url, function(err, db) {
+	  assert.equal(null, err);
+      console.log("Connected successfully to mongodb");
+	  _get(_id, db, function(res){
+	      db.close();
+ 	  	  return callback(res);
+	  });
+    });
+}
+
+function _get (_id, db, callback){
+	console.log('Executing get');
+	var collection = db.collection(db_collection);
+	// Insert a document
+	collection.findOne(_id, function(err, res){
+    	if (err) return;
+		try{
+			console.log('Get succeeded! Value: ' + res.value);
+			return callback(res.value);
+		} catch (err) {
+			return callback(new Error());
+		}
+   })
+}
+
+
+/* Hyperledger Fabric - DELETE */
+var hl_delete = exports.db_delete =  function(myobj, callback) {	
+    MongoClient.connect(url, function(err, db) {
+	  assert.equal(null, err);
+      console.log("Connected successfully to mongodb");
+	  _delete(myobj, db, function(res){
+	      db.close();
+ 	  	  return callback(res);
+	  });
+    });
+}
+
+function _delete (myobj, db, callback){
+	console.log('Executing delete');
+	var collection = db.collection(db_collection);
+	// Insert a document
+	collection.deleteOne(myobj, function(err, res){
+    	if (err){
+    		return new Error();
+    	}
+		console.log('Delete succeeded');
+		return callback("ok");
+   })
+}
