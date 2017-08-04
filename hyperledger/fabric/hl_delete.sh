82701e641327dc277f4bfbda13b0704043af0338
fede <lombardi@dis.uniroma1.it>
2017-08-04 14:18:25 +0100

    fix script hyperledger

--- hyperledger/fabric/hl_delete.sh
+++ hyperledger/fabric/hl_delete.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-CHANNEL = $1
-CHAINCODE_NAME = $2
-KEY = $3
-VALUE = $4
-DOCKER_ID = $5
+CHANNEL=$1
+CHAINCODE_NAME=$2
+KEY=$3
+VALUE=$4
+DOCKER_ID=$5
 
 docker exec $DOCKER_ID /bin/sh -c "peer chaincode invoke -o orderer0:7050  --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL -n $CHAINCODE_NAME -c '{"Args":["delete","$KEY"]}'"
 
