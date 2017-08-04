df1c9f0fb8b7b4749930fe7e8c4c19ec8b63e3ee
fede <lombardi@dis.uniroma1.it>
2017-08-04 13:56:11 +0100

    fix script get.sh

--- hyperledger/fabric/hl_get.sh
+++ hyperledger/fabric/hl_get.sh
@@ -5,6 +5,6 @@ KEY = $3
 VALUE = $4
 DOCKER_ID = $5
 
-docker exec $DOCKER_ID /bin/sh -c "peer chaincode invoke -o orderer0:7050  --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL -n $CHAINCODE_NAME -c '{"Args":["get","$KEY"]}'"
+docker exec $DOCKER_ID /bin/sh -c "peer chaincode query -o orderer0:7050  --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL -n $CHAINCODE_NAME -c '{"Args":["get","$KEY"]}'"
 
 exit 0
