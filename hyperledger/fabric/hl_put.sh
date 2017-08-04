274d4c0bf92ad73ef897eee895cb3005d56e85fc
fede <lombardi@dis.uniroma1.it>
2017-08-04 14:37:19 +0100

    fix script put

--- hyperledger/fabric/hl_put.sh
+++ hyperledger/fabric/hl_put.sh
@@ -5,6 +5,6 @@ KEY=$3
 VALUE=$4
 DOCKER_ID=$5
 
-docker exec $DOCKER_ID /bin/sh -c "peer chaincode invoke -o orderer0:7050  --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL -n $CHAINCODE_NAME -c '{"Args":["put","$KEY","$VALUE"]}'"
+docker exec $DOCKER_ID /bin/sh -c "/opt/gopath/src/github.com/hyperledger/fabric/peer/scripts/set-peer.sh 0 && /opt/gopath/src/github.com/hyperledger/fabric/peer/peer chaincode invoke -o orderer0:7050  --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL -n $CHAINCODE_NAME -c '{"Args":["put","$KEY","$VALUE"]}'"
 
 exit 0
