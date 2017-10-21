b4ea628313b80e58dc046a54eef51874514841a4
RunshanHu <runsh.hu@gmail.com>
2017-10-21 14:49:30 +0000

    implement InitChaincode.js

--- server/controllers/InitChaincode.js
+++ server/controllers/InitChaincode.js
@@ -1,5 +1,10 @@
 'use strict'
 
+var rp = require('request-promise');
+var config = require('config');
+var url = require('url');
+var hyperledger_paras = config.get('hyperledger-service');
+
 var debug = true;
 
 module.exports.initChaincode = function initChaincode(req, res, next) {
@@ -11,10 +16,44 @@ module.exports.initChaincode = function initChaincode(req, res, next) {
     "message": "example message of initChaincode"
   }
   
-  if (Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  } else {
-    res.end();
-  }
+  rp({
+      method: "POST",
+      uri: url.format({
+          protocol: 'http',
+          hostname: hyperledger_paras.host,
+          port: hyperledger_paras.port,
+          pathname: hyperledger_paras.path.init
+      }), 
+      body: {
+          "chaincodeName": req.body.chaincodeName,
+          "chaincodeVersion": req.body.chaincodeVersion,
+          "chaincodeType": req.body.chaincodeType,
+          "args": req.body.args
+      },
+      header: {
+          "authorization": req.body.authorization,
+          "content-type": "application/json"
+      },
+      json: true
+  }).then(response => {
+      if(debug) console.log(response);
+
+      if (Object.keys(examples).length > 0) {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+      } else {
+          res.end();
+      }
+  }).catch(err => {
+      if(debug) console.log("----->ServiceLedger: error when requesting hyperledger!");
+      examples['application/json'].message = "error when requesting hyperledger";
+  
+      if (Object.keys(examples).length > 0) {
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+      } else {
+          res.end();
+      }
+  });
+
 }
