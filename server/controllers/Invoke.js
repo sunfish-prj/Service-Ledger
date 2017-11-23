36fb891278920a45cfe1baeaf8a9593b5781805c
RunshanHu <runsh.hu@gmail.com>
2017-11-23 14:30:03 +0000

    add Invoke.js for home-made invoke api

---
+++ server/controllers/Invoke.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Invoke = require('../service/InvokeService');
+
+//var res = require('response');
+
+module.exports.invokePOST = function invokePOST (req, res, next) {
+  var invokeSpec = req.swagger.params['invokeSpec'].value;
+  
+  Invoke.invokePOST(invokeSpec)
+    .then(function (response) {
+      utils.writeJson(res, response, 200);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response, 400);
+    });
+};
