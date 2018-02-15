cfadeb5d588dfc6c57ddc6911c9fe73caf81495a
Andrea Margheri <margheri.andrea@gmail.com>
2018-02-15 19:35:26 +0000

    tidy up

--- server/controllers/StoreService.js
+++ server/controllers/StoreService.js
@@ -4,54 +4,49 @@
 var config = require('config');
 var out_service_name = config.get('out-service.name');
 
-
 var db_utils = require('../utils/dbUtils.js');
 var hl_utils = require('../utils/hlUtils.js');
 
 exports.deletePOST = function(args, res, next) {
 
-var response = {};
-var keyPair = args.body.value;
+  var response = {};
+  var keyPair = args.body.value;
 
-if (out_service_name == 'mongo') {
-  console.log("Calling mongo - delete");
+  if (out_service_name == 'mongo') {
+    console.log("[StoreService.js] Calling Mongo - DELETE");
 
-  db_utils.db_delete(keyPair, function (result) {
-    if (Object.keys(result).length > 0) {
-      console.log(result);
-      res.writeHead(200,{'Content-Type':'application/json'});
-      response['application/json'] = {
-        "message" : result
-       };
-      res.end(JSON.stringify(response[Object.keys(response)[0]] || {}, null, 2));
-    } else {
-      res.writeHead(400,{'Content-Type':'application/json'});
-      res.end(JSON.stringify({'message': 'error'}));
-    }
-  });
-}
+    db_utils.db_delete(keyPair, function (result) {
+      if (Object.keys(result).length > 0) {
+        res.writeHead(200,{'Content-Type':'application/json'});
+        response['application/json'] = {
+          "message" : result
+         };
+        res.end(JSON.stringify(response[Object.keys(response)[0]] || {}, null, 2));
+      } else {
+        res.writeHead(400,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({'message': 'error'}));
+      }
+    });
+  }
 
-if (out_service_name == 'fabric') {
+  if (out_service_name == 'fabric') {
 
-  console.log("Calling hyperledger");
+    console.log("[StoreService.js] Calling Hyperledger Fabric - DELETE");
+    hl_utils.hl_delete(keyPair, function (result) {
+      if (Object.keys(result).length > 0) {
+        console.log(result);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        response['application/json'] = {
+          "message" : result
+         };
+        res.end(JSON.stringify(response[Object.keys(response)[0]] || {}, null, 2));
+      } else {
+        res.writeHead(400,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({'message': 'error'}));
+      }
+    });
 
-  console.log("Calling api to 'delete' in the keyValueStore chaincode...");
-  hl_utils.hl_delete(keyPair, function (result) {
-    if (Object.keys(result).length > 0) {
-      console.log(result);
-      res.writeHead(200,{'Content-Type':'application/json'});
-      response['application/json'] = {
-        "message" : result
-       };
-      res.end(JSON.stringify(response[Object.keys(response)[0]] || {}, null, 2));
-    } else {
-      res.writeHead(400,{'Content-Type':'application/json'});
-      res.end(JSON.stringify({'message': 'error'}));
-    }
-  });
-
-}
-
+  }
 
 }
 
@@ -59,14 +54,12 @@ exports.getPOST = function(args, res, next) {
   var response = {};
   var keyPair = args.getId.value;
 
-  console.log("arguments to store: " + keyPair);
-
   if (out_service_name == 'mongo') {
-    console.log("Calling mongo - get");
+
+    console.log("[StoreService.js] Calling Mongo - GET");
 
     db_utils.db_get(keyPair, function (result) {
       if (Object.keys(result).length > 0) {
-        console.log(result);
         res.writeHead(200,{'Content-Type':'application/json'});
         response['application/json'] = {
           "message" : result
@@ -81,12 +74,9 @@ exports.getPOST = function(args, res, next) {
 
   if (out_service_name == 'fabric') {
 
-    console.log("Calling hyperledger");
-
-    console.log("Calling api to 'get' in the keyValueStore chaincode...");
+    console.log("[StoreService.js] Calling Hyperledger Fabric - GET");
     hl_utils.hl_get(keyPair, function (result) {
       if (Object.keys(result).length > 0) {
-        console.log(result);
         res.writeHead(200,{'Content-Type':'application/json'});
         response['application/json'] = {
           "message" : result
@@ -107,14 +97,12 @@ exports.putPOST = function(args, res, next) {
   var response = {};
   var keyPair = args.putSpec.value;
 
-  console.log("arguments to store: " + keyPair);
-
   if (out_service_name == 'mongo') {
-    console.log("Calling mongo - put");
+    console.log("[StoreService.js] Calling Mongo - PUT");
 
     db_utils.db_put(keyPair, function (result) {
       if (Object.keys(result).length > 0) {
-        console.log(result);
+        //console.log(result);
         res.writeHead(200,{'Content-Type':'application/json'});
         response['application/json'] = {
           "message" : result
@@ -129,12 +117,9 @@ exports.putPOST = function(args, res, next) {
 
   if (out_service_name == 'fabric') {
 
-    console.log("Calling hyperledger");
-
-    console.log("Calling api to 'put' in the keyValueStore chaincode...");
+    console.log("[StoreService.js] Calling Hyperledger Fabric - PUT");
     hl_utils.hl_put(keyPair, function (result) {
       if (Object.keys(result).length > 0) {
-        console.log(result);
         res.writeHead(200,{'Content-Type':'application/json'});
         response['application/json'] = {
           "message" : result
